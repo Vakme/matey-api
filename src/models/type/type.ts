0a1558478b0f4e471030a5d115727ec2efcb11e2
Vakme <4bielak.aleksandra@gmail.com>
2019-09-11 15:20:16 +0200

    predefined expense types

---
+++ src/models/type/type.ts
@@ -0,0 +1,34 @@
+import {prop, Typegoose} from "typegoose";
+
+class Locales {
+    @prop()
+    public pl: string;
+
+    @prop()
+    public en: string;
+}
+
+export class Type extends Typegoose {
+    @prop()
+    public name: string;
+
+    @prop()
+    public email?: string;
+
+    @prop()
+    public locales?: Locales;
+}
+
+const TypeModel = new Type().getModelForClass(Type, {
+    schemaOptions: {
+        toJSON: {
+            transform(doc, ret) {
+                delete ret._id;
+                if (ret.locales) {
+                    delete ret.locales._id;
+                }
+            }
+        }
+    }
+});
+export default TypeModel;
